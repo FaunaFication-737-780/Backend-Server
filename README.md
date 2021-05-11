5db2162875d88ff08c74831494054b9fa4a61f10
Ray Guo <ma2808203259@hotmail.com>
2021-05-11 18:05:44 +0800

    init set up

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Template
-template for sit725 practice 
+# SIT737&SIT780 Backend service
+## Introduction 
+This service is used to process the backend data, and when front end send request, provide the data. 
+
+## The items for this service
+- IBM Watson APIs
+- Data for animals
+- Watch the data changes
+- etc...
+
+## How to run it
+```
+npm install
+npm start
+```
+The application is listening on [Localhost:3002](http://localhost:3002)
--- server.js
+++ server.js
@@ -1,38 +1,31 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const mongoose = require("mongoose")
 
 const app = express()
-const PORT = 3000
-//mongodb uri
-//const uri = "mongodb+srv://ray:1998@cluster0.ho33k.mongodb.net/Sitboard?retryWrites=true&w=majority";
-const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/Sitboard?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
+const PORT = process.env.PORT || 3002
 
-const client = new MongoClient(uri, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
+const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/test?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
+//mongoose.connect('mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
-//connect to the database
-let journalsCollection
-const openConnection = () => {
-  client.connect(err => {
-    journalsCollection = client.db("Sitboard").collection("journals");
-    if (!err) {
-      console.log('connected to the database');
-    }
+let db = mongoose.connection;
 
-  })
-}
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+db.once('open', function() {
+    // we're connected!
+    console.log("database connected")
+
+});
+
+
+
+
+
 app.use(express.static('public'))
 
 
-app.get('', function(req, res){
-    
-
-})
-
-openConnection()
 
 app.listen(PORT,()=>{
-    console.log('server started on port 3000')
+    console.log('server started on port '+PORT)
 })
