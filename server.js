e8d425a928d81debcb21651d09fc09268d0a68d9
Ray Guo <ma2808203259@hotmail.com>
2021-05-27 20:35:22 +0800

    change the database url

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const routers = require('./routes/donateRoutes')
 
 const PORT = process.env.PORT || 3002
 
-//const uri ='mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata'
-const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/test?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
+const uri ='mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata'
+//const uri = "mongodb://ray:1998@cluster0-shard-00-00.ho33k.mongodb.net:27017,cluster0-shard-00-01.ho33k.mongodb.net:27017,cluster0-shard-00-02.ho33k.mongodb.net:27017/test?ssl=true&replicaSet=atlas-k8w5gq-shard-0&authSource=admin&retryWrites=true&w=majority";
 //mongoose.connect('mongodb+srv://admin:admin@cluster0.5cdt0.mongodb.net/geodata', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect(uri, {
     useNewUrlParser: true,
